b20310c26788c2a3537f4785a43f52602d8a8892
Nicolas Hemenway <nhemenway@catalystsolves.com>
2020-11-05 10:33:36 -0500

    Initial commit

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './App.module.css';
+
+/**
+ * 
+ * @param {*} props 
+ */
+const App = (props) => {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
