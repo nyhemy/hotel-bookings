bb82ad8f0c37941e4a69512d959441cc3292d9d3
Nicolas Hemenway <nhemenway@catalystsolves.com>
2020-11-16 16:11:56 -0500

    fixed bug in Reservation with getRoomType for loop, now it's pulling RoomType info from API correctly and displaying the actual room type rather than a number

--- src/components/reservation/Reservation.js
+++ src/components/reservation/Reservation.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Reservation.module.css';
 import RoomTypes from '../roomTypes/RoomTypes';
+
 const Reservation = (props) => {
     const {checkInDate, guestEmail, id, numberOfNights, roomTypeId, user} = props;
     const axios = require('axios').default;
@@ -8,36 +9,46 @@ const Reservation = (props) => {
     const [rooms, setRooms] = useState([]);
 
     useEffect (() => {
-    
-            // getRoomTypes();
-        }, []);
 
-    // const getRoomTypes = () => {
-    //     axios.get('http://localhost:8080/room-types', {
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //             'mode': 'cors',
-    //             'Authorization': `Bearer ${sessionStorage.getItem('token')}`
-    //         }
-    //     })
-    //     .then(response => {
-    //         const res = response.data;
-    //         console.log(res);
-    //         setRooms(res);
-    //     })
-    //     .catch(error => {
-    //         console.log(error);
-    //     });
-    // }
+        getRoomTypes();
+    }, []);
+
+    const getRoomTypes = () => {
+        axios.get('http://localhost:8080/room-types', {
+            headers: {
+                'Content-Type': 'application/json',
+                'mode': 'cors',
+                'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+            }
+        })
+        .then(response => {
+            const res = response.data;
+            console.log(res);
+            setRooms(res);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+    const test = () => {
+        console.log(rooms);
+    }
 
     const getRoomType = (num) => {
-        return rooms[num];
+        let r = rooms;
+        for (let i = 0; i <= r.length-1; i++) {
+            if (r[i].id === num) {
+                return r[i].name;
+            }
+        }
     }
 
     return (
         <div className={styles.card}>
             <div>Guest: {guestEmail}</div>
             <div>Room type: {getRoomType(roomTypeId)}</div>
+            {/* <div>Room type: test</div> */}
             <div>CheckIn date: {checkInDate}</div>
             <div>Number of nights: {numberOfNights}</div>
         </div>
