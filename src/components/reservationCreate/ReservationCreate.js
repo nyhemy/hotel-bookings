73421dc57b5217ab882e27428a4998e23a26a289
Nicolas Hemenway <nhemenway@catalystsolves.com>
2020-12-18 17:49:12 -0500

    added onChange handling for ReservationCreate

--- src/components/reservationCreate/ReservationCreate.js
+++ src/components/reservationCreate/ReservationCreate.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import styles from './ReservationCreate.module.css';
 import Button from '../button/Button';
 
 const ReservationCreate = () => {
 
     const axios = require('axios').default;
+    const history = useHistory();
+
+    const [email, setEmail] = useState('');
+    const [date, setDate] = useState('');
+    const [numNights, setNumNights] = useState('');
+    const [room, setRoom] = useState('');
+
+    useEffect(() => {
+        if (!sessionStorage.getItem("token")) {
+            history.push("/");
+        }
+        //check API for roomtypes to be used in select dropdown in form
+    }, []);
 
 
-    const handleChange = () => {
-
+    const handleChange = (event) => {
+        event.preventDefault();
+        switch (event.target.name) {
+            case "email":
+                setEmail(event.target.value);
+                break;
+            case "date":
+                setDate(event.target.value);
+                break;
+            case "numNights":
+                setNumNights(event.target.value);
+                break;
+            case "room":
+                setRoom(event.target.value);
+                break;
+            default:
+                break;
+        }      
     }
 
     const handleSubmit = () => {
@@ -21,12 +51,12 @@ const ReservationCreate = () => {
 
     return (
         <div className={styles.container}>
-            <form className={styles.form} id='ReservationCreate' onSubmit={handleSubmit} noValidate>
+            <form className={styles.form} onSubmit={handleSubmit} noValidate>
                 <div className={styles.input}><input type='email' name='email' placeholder='email' onChange={handleChange} /></div> 
-                <div className={styles.input}><input type='text' name='check-in date' placeholder='check-in date' onChange={handleChange} /></div>
-                <div className={styles.input}><input type='number' name='number of nights' placeholder='number of nights' onChange={handleChange} /></div>
+                <div className={styles.input}><input type='text' name='date' placeholder='check-in date' onChange={handleChange} /></div>
+                <div className={styles.input}><input type='number' name='numNights' placeholder='number of nights' onChange={handleChange} /></div>
                 <div>
-                    <select className={styles.select} name='room type' form='ReservationCreate'>
+                    <select className={styles.select} name='room' onChange={handleChange}>
                         <option>--select room--</option>
                         <option value="test1">test1</option>
                         <option value="test2">test2</option>
