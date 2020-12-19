ec1a3c65e33ec6c36f0e8b24c69669e8915a7a90
Nicolas Hemenway <nhemenway@catalystsolves.com>
2020-12-19 17:24:37 -0500

    added get request for room-types in ReservationCreate. Added loading and error requirements

--- src/components/reservationCreate/ReservationCreate.js
+++ src/components/reservationCreate/ReservationCreate.js
@@ -2,11 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import styles from './ReservationCreate.module.css';
 import Button from '../button/Button';
+import loadImg from '../ajax-loader.gif';
 
 const ReservationCreate = () => {
 
     const axios = require('axios').default;
     const history = useHistory();
+
+    const [rooms, setRooms] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
 
     const [email, setEmail] = useState('');
     const [date, setDate] = useState('');
@@ -17,8 +22,29 @@ const ReservationCreate = () => {
         if (!sessionStorage.getItem("token")) {
             history.push("/");
         }
-        //check API for roomtypes to be used in select dropdown in form
+        
+        getRoomTypes();
     }, []);
+
+    const getRoomTypes = () => {
+        setError(false);
+        setLoading(true);
+        axios.get('http://localhost:8080/room-types', {
+            headers: {
+                'Content-Type': 'application/json',
+                'mode': 'cors',
+                'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+            }
+        })
+        .then(response => {
+            setLoading(false);
+            setRooms(response.data);
+        })
+        .catch(error => {
+            setLoading(false);
+            setError(true);
+        });
+    }
 
 
     const handleChange = (event) => {
@@ -45,27 +71,30 @@ const ReservationCreate = () => {
 
     }
 
-    const createReservation = () => {
-
-    }
-
     return (
         <div className={styles.container}>
-            <form className={styles.form} onSubmit={handleSubmit} noValidate>
-                <div className={styles.input}><input type='email' name='email' placeholder='email' onChange={handleChange} /></div> 
-                <div className={styles.input}><input type='text' name='date' placeholder='check-in date' onChange={handleChange} /></div>
-                <div className={styles.input}><input type='number' name='numNights' placeholder='number of nights' onChange={handleChange} /></div>
-                <div>
-                    <select className={styles.select} name='room' onChange={handleChange}>
-                        <option>--select room--</option>
-                        <option value="test1">test1</option>
-                        <option value="test2">test2</option>
-                        <option value="test3">test3</option>
-                        <option value="test4">test4</option>
-                    </select>
-                    <Button onClick={createReservation}>Create</Button>
-                </div>
-            </form>
+            {error && <h3 className={styles.error}>Oops something went wrong</h3>}
+            {loading ?
+                <img src={loadImg} alt="loading..." />
+            :
+                !error && <>
+                    <form className={styles.form} onSubmit={handleSubmit} noValidate>
+                        <div className={styles.input}><input type='email' name='email' placeholder='email' onChange={handleChange} /></div> 
+                        <div className={styles.input}><input type='text' name='date' placeholder='check-in date' onChange={handleChange} /></div>
+                        <div className={styles.input}><input type='number' name='numNights' placeholder='number of nights' onChange={handleChange} /></div>
+                        <div>
+                            <select className={styles.select} name='room' onChange={handleChange}>
+                                <option>--select room--</option>
+                                <option value="test1">test1</option>
+                                <option value="test2">test2</option>
+                                <option value="test3">test3</option>
+                                <option value="test4">test4</option>
+                            </select>
+                            <button type='submit'>Create</button>
+                        </div>
+                    </form>
+                </>
+            }
         </div>
     );
 }
