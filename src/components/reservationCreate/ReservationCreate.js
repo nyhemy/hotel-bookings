78fb360d5eb5c58f5400802283cf7b48eeddff5f
Nicolas Hemenway <nhemenway@catalystsolves.com>
2020-12-19 17:39:36 -0500

    added mapping for ReservationCreate dropdown options

--- src/components/reservationCreate/ReservationCreate.js
+++ src/components/reservationCreate/ReservationCreate.js
@@ -83,13 +83,11 @@ const ReservationCreate = () => {
                         <div className={styles.input}><input type='text' name='date' placeholder='check-in date' onChange={handleChange} /></div>
                         <div className={styles.input}><input type='number' name='numNights' placeholder='number of nights' onChange={handleChange} /></div>
                         <div>
-                            <select className={styles.select} name='room' onChange={handleChange}>
-                                <option>--select room--</option>
-                                <option value="test1">test1</option>
-                                <option value="test2">test2</option>
-                                <option value="test3">test3</option>
-                                <option value="test4">test4</option>
-                            </select>
+                        <select className={styles.select} name='room' onChange={handleChange}>
+                            {rooms.map((data, index) => <option className={styles.select} key={index}>
+                                {data.name}
+                            </option>)}
+                        </select>
                             <button type='submit'>Create</button>
                         </div>
                     </form>
