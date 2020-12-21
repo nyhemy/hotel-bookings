cce7a3f06426a5787b1394c437f22b506b083974
Nicolas Hemenway <nhemenway@catalystsolves.com>
2020-12-21 13:50:42 -0500

    got api get request for specific reservation working

--- src/components/reservationEdit/ReservationEdit.js
+++ src/components/reservationEdit/ReservationEdit.js
@@ -8,8 +8,11 @@ const ReservationEdit = () => {
     const history = useHistory();
     let { id } = useParams();
 
-    const [ids, setIds] = useState([]);
 
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [reservation, setReservation] = useState();
+    
     useEffect(() => {
         if (!sessionStorage.getItem("token")) {
             history.push("/");
@@ -19,7 +22,23 @@ const ReservationEdit = () => {
     }, []);
 
     const getReservation = () => {
-        
+        setError(false);
+        setLoading(true);
+        axios.get('http://localhost:8080/reservations/' + id, {
+            headers: {
+                'Content-Type': 'application/json',
+                'mode': 'cors',
+                'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+            }
+        })
+        .then(response => {
+            setLoading(false);
+            setReservation(response.data);
+        })
+        .catch(error => {
+            setLoading(false);
+            setError(true);
+        });
     }
 
     return (
