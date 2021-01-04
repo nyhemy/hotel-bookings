73fe8351a424a7c984b8d0537fc0a80158cb160f
Nicolas Hemenway <nhemenway@catalystsolves.com>
2021-01-04 11:46:45 -0500

    got tests to 60

--- src/tests/RoomCreate.test.js
+++ src/tests/RoomCreate.test.js
@@ -1,18 +1,38 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 // import { unmountComponentAtNode } from 'react-dom';
 import RoomCreate from '../components/roomCreate/RoomCreate';
 import '@testing-library/jest-dom/extend-expect';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
-import 'react-test-renderer';
+import renderer from 'react-test-renderer';
+
+const history = createMemoryHistory();
 
 test('renders w/o crashing', () => {
-    const history = createMemoryHistory();
 
     render(
         <Router history={history}>
             <RoomCreate />
         </Router>,
     );
+});
+
+test('app component matches snapshot', () => {
+    const component = renderer.create(<RoomCreate />);
+    let tree = component.toJSON();
+  
+    expect(tree).toMatchSnapshot();
+  
+});
+
+test('verify that form invalidates', () => {
+
+    const{ getByText } = render(<Router history={history}><RoomCreate /></Router>);
+  
+    fireEvent.click(getByText('Create'));
+  
+    expect(getByText('Must be at least 3 characters')).toBeInTheDocument();
+    expect(getByText('Must be number greater than zero')).toBeInTheDocument();
+  
 });
