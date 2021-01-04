a533645ccf914a0713f6e7310ded98205ad8839a
Nicolas Hemenway <nhemenway@catalystsolves.com>
2021-01-04 10:40:28 -0500

    got prototype tests working

--- src/tests/RoomCreate.test.js
+++ src/tests/RoomCreate.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { unmountComponentAtNode } from 'react-dom';
+// import { unmountComponentAtNode } from 'react-dom';
 import RoomCreate from '../components/roomCreate/RoomCreate';
 import '@testing-library/jest-dom/extend-expect';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
 
 it('renders w/o crashing', () => {
-  const div = document.createElement('div');
-  render(<RoomCreate />, div);
-  unmountComponentAtNode(div);
+    const history = createMemoryHistory();
+    render(
+        <Router history={history}>
+            <RoomCreate />
+        </Router>
+    );
 });
--- src/tests/RoomTypes.test.js
+++ src/tests/RoomTypes.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { unmountComponentAtNode } from 'react-dom';
+// import { unmountComponentAtNode } from 'react-dom';
 import RoomTypes from '../components/roomTypes/RoomTypes';
 import '@testing-library/jest-dom/extend-expect';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
 
 it('renders w/o crashing', () => {
-    const div = document.createElement('div');
-    render(<RoomTypes />, div);
-    unmountComponentAtNode(div);
+    const history = createMemoryHistory();
+    render(
+        <Router history={history}>
+            <RoomTypes />
+        </Router>
+    );
 });
